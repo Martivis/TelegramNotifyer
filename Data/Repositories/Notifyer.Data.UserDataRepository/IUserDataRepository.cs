50ed460c1cb0f8b45e4bc801398730fa8bdebff1
Martivis <vanek-non@rambler.ru>
2023-07-24 02:07:02 +0300

    Add UserData Repository

---
+++ Data/Repositories/Notifyer.Data.UserDataRepository/IUserDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using Notifyer.Data.Context.Entities;
+
+namespace Notifyer.Data.UserDataRepository
+{
+    public interface IUserDataRepository
+    {
+        Task AddUserAsync(UserData userData);
+        Task<IEnumerable<UserData>> GetAllUsersAsync();
+        Task<UserData> GetUserAsync(int id);
+        Task UpdateUserAsync(UserData userData);
+        Task DeleteUserAsync(int id);
+    }
+}
---
+++ Data/Repositories/Notifyer.Data.UserDataRepository/UserDataRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notifyer.Data.Context;
+using Notifyer.Data.Context.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifyer.Data.UserDataRepository
+{
+    internal class UserDataRepository : IUserDataRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserDataRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUserAsync(UserData userData)
+        {
+            _context.Add(userData);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            var user = await GetUserAsync(id);
+            _context.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<UserData>> GetAllUsersAsync()
+        {
+            return await _context.Set<UserData>().ToListAsync();
+        }
+
+        public async Task<UserData> GetUserAsync(int id)
+        {
+            return await _context.FindAsync<UserData>(id)
+                ?? throw new ApplicationException($"User with id {id} not found");
+        }
+
+        public async Task UpdateUserAsync(UserData userData)
+        {
+            _context.Update(userData);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
