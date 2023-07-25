b4add5914c1a62ba623dd870949f9474436d3577
Martivis <vanek-non@rambler.ru>
2023-07-25 12:27:47 +0300

    Add NewsCathegoryRepository

---
+++ Data/Repositories/Notifyer.Data.NewsCathegoryRepository/INewsCathegoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Notifyer.Data.Context.Entities;
+
+namespace Notifyer.Data.NewsCathegoryRepository
+{
+    public interface INewsCathegoryRepository
+    {
+        Task AddAsync(NewsCathegory cathegory);
+        Task<IEnumerable<NewsCathegory>> GetAllAsync();
+        Task<IEnumerable<NewsCathegory>> GetByUserAsync(int id);
+        Task<NewsCathegory> GetAsync(int id);
+        Task UpdateAsync(NewsCathegory cathegory);
+        Task DeleteAsync(int id);
+    }
+}
