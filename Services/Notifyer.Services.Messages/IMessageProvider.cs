bea6155e4b11d321ebef438ce15c689dcbaa35fc
Martivis <vanek-non@rambler.ru>
2023-07-31 18:25:33 +0300

    Add messages service

---
+++ Services/Notifyer.Services.Messages/IMessageProvider.cs
@@ -0,0 +1,14 @@
+﻿using Notifyer.Services.KafkaDataProvider.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifyer.Services.Messages
+{
+    public interface IMessageProvider
+    {
+        string GetMessage(NewsModel model);
+    }
+}
---
+++ Services/Notifyer.Services.Messages/MessageProvider.cs
@@ -0,0 +1,17 @@
+﻿using Notifyer.Services.KafkaDataProvider.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifyer.Services.Messages
+{
+    internal class MessageProvider : IMessageProvider
+    {
+        public string GetMessage(NewsModel model)
+        {
+            return $"{model.Title}\n\n{model.Content}\n\n{model.Cathegory}";
+        }
+    }
+}
