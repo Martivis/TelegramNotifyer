4737deb2cf52bf26ecebc81b51c59cd97c06d6b8
Martivis <vanek-non@rambler.ru>
2023-08-09 10:58:11 +0300

    Add news provider bootstraper

---
+++ Services/Notifyer.Services.KafkaDataProvider/Bootstraper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifyer.Services.KafkaDataProvider
+{
+    public static class Bootstraper
+    {
+        public static IServiceCollection AddMockNewsProvider(IServiceCollection services)
+        {
+            services.AddSingleton<INewsProvider, MockNewsProvider>();
+            return services;
+        }
+    }
+}
