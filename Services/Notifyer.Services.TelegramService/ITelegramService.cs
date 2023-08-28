684cb4c522dfee5da944d12519b6ddc158b9a6f9
Martivis <vanek-non@rambler.ru>
2023-08-28 22:46:36 +0300

    Add TelegramService

---
+++ Services/Notifyer.Services.TelegramService/ITelegramService.cs
@@ -0,0 +1,8 @@
+﻿namespace Notifyer.Services.TelegramService
+{
+    public interface ITelegramService
+    {
+        void Start();
+        Task SendMessageAsync(long chatId, string message);
+    }
+}
---
+++ Services/Notifyer.Services.TelegramService/TelegramService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Notifyer.Services.TelegramService
+{
+    internal class TelegramService : ITelegramService
+    {
+        private readonly TelegramBotClient _client;
+        private CancellationTokenSource _tokenSource;
+        private ReceiverOptions _receiverOptions;
+
+        public TelegramService(string token)
+        {
+            _client = new TelegramBotClient(token);
+            _tokenSource = new CancellationTokenSource();
+            _receiverOptions = new ReceiverOptions()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            };
+        }
+
+        public void Start()
+        {
+            _client.StartReceiving(receiverOptions: _receiverOptions,
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandleErrorAsync,
+                cancellationToken: _tokenSource.Token);
+        }
+
+        private async Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken cancellationToken)
+        {
+            var message = update.Message;
+
+        }
+
+        private async Task HandleErrorAsync(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+
+        }
+
+        public async Task SendMessageAsync(long chatId, string message)
+        {
+            await _client.SendTextMessageAsync(chatId, message);
+        }
+    }
+}
